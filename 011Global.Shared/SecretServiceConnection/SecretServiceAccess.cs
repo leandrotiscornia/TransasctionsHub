335b19da3b58baa0c69e8db980737313bfc5c09c
Leandro <leandrotiscornia98@gmail>
2025-02-08 20:53:47 -0300

    Added hardcoded secret service access, and encryption utility class

---
+++ 011Global.Shared/SecretServiceConnection/SecretServiceAccess.cs
@@ -0,0 +1,66 @@
+﻿using _011Global.Shared.JobsServiceDBContext.Entities;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _011Global.Shared.SecretServiceConnection
+{
+    public static class SecretServiceAccess
+    {
+        /// <summary>
+        /// This should access a Secret Manager service, it could be Hashicorp's Vault, Keycloak, AWS secret manager 
+        /// or any other. This is just a hard-coded class, the real service should be deployed on an internal network
+        /// with several access restriction.
+        /// </summary>
+        public static string SecretServiceUrl { get; set; }
+
+
+        public static async Task<string> GetUSAEpayKeyPin( )
+        {
+            string key = "_1v60HeN4Wpe03jK2rcZ8Q6u309MRj48";
+            Console.WriteLine($"Key for UsaEpayAPI retrieved from secret's service");
+            return key;
+        }
+
+        public static async Task<string> GetUSAEpayKey( )
+        {
+            string pin = "sdk5G4";
+            Console.WriteLine($"Pin for UsaEpayAPI retrieved from secret's service");
+            return pin;
+        }
+
+        public static async Task<string> GetDatabaseUserName(string database)
+        {
+            string user = "";
+            Console.WriteLine($"Username for database {database} retrieved from secret's service");
+            return user;
+        }
+        public static async Task<string> GetDatabasePassword(string database)
+        {
+            string password = "";
+            Console.WriteLine($"Password for database {database} retrieved from secret's service");
+            return password;
+        }
+        public static async Task<string> GetAESEncryptionKey()
+        {
+            string key = "w9z8k+WIOtb/vVafrcjFFTdNLFul9bZRvkwulZzUP5Q=";
+            Console.WriteLine("Encryption key retrieved from secret's server");
+            return key;
+        }
+
+        public static async Task<string> GetAESCreditCardIV(int globalCreditCardId) 
+        {
+            string iv = "5TGbbhQxsiWXsi6W9fP3dQ==";
+            Console.WriteLine($"Initialization vector for {globalCreditCardId} retrieved from secret's server");
+            return iv;
+        }
+
+        public static async Task SaveAESCreditCardIV(string iv, int globalCreditCardId)
+        {
+            Console.WriteLine($"Initialization vector for {globalCreditCardId} saved in secret's server");
+        }
+    }
+}
---
+++ 011Global.Shared/Utilities/EncryptionUtility.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _011Global.Shared.SecretServiceConnection;
+using System.Security.Cryptography;
+using _011Global.Shared.JobsServiceDBContext.Entities;
+
+namespace _011Global.Shared.Utilities
+{
+    public static class EncryptionUtility
+    {
+
+        public static async Task<string> AES256GenerateInicializationVector(int id)
+        {
+            string iv;
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = 256;
+                aes.GenerateIV();
+                iv = Convert.ToBase64String(aes.IV);
+            }
+            await SecretServiceAccess.SaveAESCreditCardIV(iv, id);
+            return iv;
+        }
+
+        public static string AES256Encrypt(string toEncrypt, string key, string iv)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Convert.FromBase64String(key);
+                aes.IV = Convert.FromBase64String(iv);
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                byte[] encryptedBytes;
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        byte[] bytes = Encoding.UTF8.GetBytes(toEncrypt);
+                        cs.Write(bytes, 0, bytes.Length);
+                    }
+                    encryptedBytes = ms.ToArray();
+
+                }
+                return Convert.ToBase64String(encryptedBytes);
+            }
+        }
+        public static string AES256Decrypt(string toDecrypt, string key, string iv)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Convert.FromBase64String(key);
+                aes.IV = Convert.FromBase64String(iv);
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                byte[] bytesToDecrypt = Convert.FromBase64String(toDecrypt);
+                byte[] decryptedBytes;
+                using (var ms = new MemoryStream(bytesToDecrypt))
+                {
+                    using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var msText = new MemoryStream())
+                        {
+                            cs.CopyTo(msText);
+                            decryptedBytes = msText.ToArray();
+                        }
+                    }
+
+                }
+                return Encoding.UTF8.GetString(decryptedBytes);
+            }
+        }
+    }
+}
